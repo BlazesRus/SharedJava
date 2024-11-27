7a84150a9f9a3f93d4a9f5643b2cff5ebeb68eda
BlazesRus (James) <BlazesRus@yahoo.com>
2024-11-27 00:12:25 -0500

    Reorganizing to be closer to my c++ version

--- AltNum/MediumDec/MediumUDec.java
+++ AltNum/MediumDec/MediumUDec.java
@@ -1,66 +1,79 @@
 ﻿import java.math.BigInteger;
 
-// Represents 0 - 4294967295.999999999 with 100% consistency of accuracy for most operations as long as don't get too small
-// (8 bytes worth of Variable Storage inside class for each instance)
-public class MediumUDec {
-    
-	//Stores whole half of number(value range 0 - 4294967295)
-    private int IntHalf;
-	
-	//Stores decimal section info and other special info(value range 0-999999999)
-    private int DecimalHalf;
+package BlazesRusCode.AltNum;
 
-    // The decimal overflow
-    public static final int DecimalOverflow = 1000000000;
+	// Represents 0 - 4294967295.999999999 with 100% consistency of accuracy for most operations as long as don't get too small
+	// (8 bytes worth of Variable Storage inside class for each instance)
+	public class MediumUDec
+	{
+		
+	//region DigitStorage
 
-    // The decimal overflow
-    public static final long DecimalOverflowX = 1000000000;
+		//Stores whole half of number(value range 0 - 4294967295)
+		private int IntHalf;
+		
+		//Stores decimal section info and other special info(value range 0-999999999)
+		private int DecimalHalf;
 
-    // Initializes a new instance of the MediumUDec class.
-    public MediumUDec(int intVal, int decVal) {
-        this.IntHalf = intVal;
-        this.DecimalHalf = decVal;
-    }
+		// Return IntHalf as signed int
+		public int GetIntHalf() {
+			return IntHalf;
+		}
 
-    public MediumUDec() {
-        this(0, 0);
-    }
+		// Return DecimalHalf as signed int
+		public int GetDecHalf() {
+			return DecimalHalf;
+		}
 
-    // Return IntHalf as unsigned int
-    public int GetIntHalf() {
-        return IntHalf;
-    }
+	//endregion DigitStorage
 
-    // Return DecimalHalf as unsigned char
-    public int GetDecHalf() {
-        return DecimalHalf;
-    }
+		// The decimal overflow
+		public static final int DecimalOverflow = 1000000000;
 
-    public void SetValue(MediumUDec rValue) {
-        this.IntHalf = rValue.IntHalf;
-        this.DecimalHalf = rValue.DecimalHalf;
-    }
+		// The decimal overflow
+		public static final long DecimalOverflowX = 1000000000;
 
-    public MediumUDec Initialize(int intVal, int decVal) {
-        return new MediumUDec(intVal, decVal);
-    }
+	//region class_constructors
 
-    public MediumUDec getVariant(MediumUDec variantValue) {
-        return new MediumUDec(variantValue.IntHalf, variantValue.DecimalHalf);
-    }
+		// Initializes a new instance of the MediumUDec class.
+		public MediumUDec(int intVal, int decVal) {
+			this.IntHalf = intVal;
+			this.DecimalHalf = decVal;
+		}
 
-    public MediumUDec assign(MediumUDec rhs) {
-        if (this == rhs) {
-            return this; // Check for self-assignment
-        }
-        this.IntHalf = rhs.IntHalf;
-        this.DecimalHalf = rhs.DecimalHalf;
-        return this;
-    }
+		public MediumUDec() {
+			this(0, 0);
+		}
 
-    public MediumUDec assign(int rhs) {
-        this.IntHalf = rhs;
-        this.DecimalHalf = 0;
-        return this;
-    }
-}
+		public MediumUDec assign(MediumUDec rhs) {
+			if (this == rhs) {
+				return this; // Check for self-assignment
+			}
+			this.IntHalf = rhs.IntHalf;
+			this.DecimalHalf = rhs.DecimalHalf;
+			return this;
+		}
+
+		public MediumUDec assign(int rhs) {
+			this.IntHalf = rhs;
+			this.DecimalHalf = 0;
+			return this;
+		}
+
+		public void SetValue(MediumUDec rValue) {
+			this.IntHalf = rValue.IntHalf;
+			this.DecimalHalf = rValue.DecimalHalf;
+		}
+
+		public MediumUDec Initialize(int intVal, int decVal) {
+			return new MediumUDec(intVal, decVal);
+		}
+
+		public MediumUDec getVariant(MediumUDec variantValue) {
+			return new MediumUDec(variantValue.IntHalf, variantValue.DecimalHalf);
+		}
+
+	//endregion class_constructors
+
+
+	}
